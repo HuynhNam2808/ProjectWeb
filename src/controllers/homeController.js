bee0a95d96872b659a96fd3e1859108f8eaec8d3
Huynh Nam <huynhnamdvt123@gmail.com>
2023-02-17 15:56:41 +0700

    fifth commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -26,10 +26,38 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------');
-    console.log(data);
-    console.log('---------------------');
     return res.render('displayCRUD.ejs', { dataTable: data });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        //check user data not found
+        //let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('User not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', { dataTable: allUsers });
+}
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    await CRUDService.deleteUserById(id);
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('delete user succeed!');
+    } else {
+        return res.send('User not found');
+    }
 }
 module.exports = {
     getHomePage: getHomePage,
@@ -37,4 +65,7 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,9 +5,14 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
-    router.get('/curd', homeController.getCRUD);
+    router.get('/crud', homeController.getCRUD);
+
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-curd', homeController.displayGetCRUD);
+    router.get('/edit-curd', homeController.getEditCRUD);
+
+    router.post('/put-curd', homeController.putCRUD);
+    router.get('/delete-curd', homeController.deleteCRUD);
     //REST API
     return app.use("/", router);
 }
